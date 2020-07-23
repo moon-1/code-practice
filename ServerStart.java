a8e76a9a810fee24b52fdc73e1c650f0e48dea27
HAE WON <52816832+moon-1@users.noreply.github.com>
2020-07-23 22:02:25 +0900

    Simple Web Server for test

---
+++ ServerStart.java
@@ -0,0 +1,13 @@
+package com.company.httpServer.main;
+
+import com.company.httpServer.module.HttpServerModule;
+
+public class ServerStart {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("HTTP Server Start!!");
+        new HttpServerModule();
+    }
+
+}
