718c815dba45630ed00ce5c8c5e26acdbce182ff
HAE WON <52816832+moon-1@users.noreply.github.com>
2020-07-27 19:38:55 +0900

    Android Retrofit POST

---
+++ Post.java
@@ -0,0 +1,43 @@
+package myapplication.httpurlconnection;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Post {
+    /**
+     * {
+     *     "userId": 1,
+     *     "id": 1,
+     *     "title": "sunt aut facere repellat ~~~",
+     *     "body": "quia et suscipit\nsuscipit ~~~"
+     * },
+     */
+
+    private int userId;
+    private Integer id;
+    private String title;
+    @SerializedName("body")
+    private String text;
+
+    public Post(int userId, String title, String text) {
+        this.userId = userId;
+        this.title = title;
+        this.text = text;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
+
