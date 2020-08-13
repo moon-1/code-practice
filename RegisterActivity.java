e702c1a4461fbc498c3fb06829e1d7a37ab999ef
HAE WON <52816832+moon-1@users.noreply.github.com>
2020-08-13 22:19:20 +0900

    client activity class

---
+++ RegisterActivity.java
@@ -0,0 +1,17 @@
+package com.practicecode.login2;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.practicecode.login2.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        
+    }
+}
