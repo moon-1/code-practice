a4c34ca613b87aea85c3394cb6bd347fa7644573
HAE WON <52816832+moon-1@users.noreply.github.com>
2020-08-22 12:50:23 +0900

    Baekjoon Q.9093

---
+++ baekjoon_9093.cpp
@@ -0,0 +1,30 @@
+//단어 뒤집기
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    cin.ignore();
+    while(t--){
+        string str;
+        getline(cin,str);
+        str +='\n';
+        stack<char> s;
+        for(char ch : str){
+            if(ch=='.'||ch=='\n'){
+                while(!is.empty()){
+                    cout << s.top();
+                    s.pop();
+                }
+                cout << ch;
+            }else{
+                s.push(ch);
+            }
+        }
+    }
+    return 0;
+}
