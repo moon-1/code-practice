914281b6905fc674ca4eb089f934cb4f6f61e23f
HAE WON <52816832+moon-1@users.noreply.github.com>
2020-10-11 14:33:18 +0900

    Baekjoon Q.14226

---
+++ baekjoon_14226.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <tuple>
+#include <queue>
+#include <cstring>
+using namespace std;
+int d[1001][1001];
+int main(){
+    int n;
+    cin >> n;
+    memset(d,-1,sizeof(d));
+    queue<pair<int,int>> q;
+    q.push(make_pair(1,0));
+    d[1][0] = 0;
+    while(!q.empty()){
+        int s,c;//s: 스티커 갯수, c: 클립보드 스티커갯수
+        tie(s,c) = q.front();
+        q.pop();
+        if(d[s][s] ==-1){
+            d[s][s] = d[s][c] + 1;
+            q.push(make_pair(s,s));
+        }
+        if(s+c <= n && d[s+c][c] == -1){
+            d[s+c][c] = d[s][c] + 1;
+            q.push(make_pair(s+c,c));
+        }
+        if(s-1 >=0 && d[s-1][c]==-1){
+            d[s-1][c] = d[s][c] + 1;
+            q.push(make_pair(s-1,c));
+        }
+    }
+
+    int ans = -1;
+    for(int i=0;i<=n;i++){
+        if(d[n][i] != -1){
+            if(ans == -1 || ans > d[n][i]){
+                ans = d[n][i];
+            }
+        }
+    }
+    cout << ans <<'\n';
+    return 0;
+}
