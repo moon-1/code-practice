e544d7b453852a51948600d15ce6ad980379208a
moon-1 <mhwgodnjs@naver.com>
2021-09-16 13:25:51 +0900

    Repository Organizing

--- Makefile
+++
@@ -1,4 +0,0 @@
-scheduler: myscheduler.o Queue.o
-	gcc -lm -o scheduler myscheduler.o Queue.o
-myscheduler.o : myscheduler.c Queue.c Queue.h
-	gcc -c myscheduler.c
--- RetrofitAPI.java
+++
@@ -1,9 +0,0 @@
-package org.techtown.login;
-
-import retrofit2.Call;
-import retrofit2.http.GET;
-
-public interface RetrofitAPI {
-    @GET("/userList")
-    Call<String> getUserList();
-}
